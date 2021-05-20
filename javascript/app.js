c57b3613c073017cbae94ad29e0dfc20faea2e86
et77 <81599370+pon56@users.noreply.github.com>
2021-05-20 21:28:53 +0900

    完成

--- javascript/app.js
+++ javascript/app.js
@@ -79,6 +79,35 @@ $("#js-form input, #js-form textarea").on("change",function(){
     }
 })
 
+// 送信の成否を判定
+let $form = $("#js-form")
+$form.submit(function (e) {
+    $.ajax({
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+            0: function () {
+                //送信に成功したときの処理 
+                $form.slideUp()
+                $("#js-success").slideDown()
+            },
+            200: function () {
+                //送信に失敗したときの処理 
+                $form.slideUp()
+                $("#js-err").slideDown()
+            }
+        }
+    });
+    return false;
+});
+
 // WOW起動
 new WOW().init();
+
+// drawer.js起動
+$(document).ready(function() {
+	 $('.drawer').drawer();
+});
 
